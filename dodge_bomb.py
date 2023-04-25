d5bf714e2c503ce4375add182ddf6d6711cb7322
ahmed <c0b21010af@edu.teu.ac.jp>
2023-04-25 16:59:05 +0900

    1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,7 @@ import random
 import sys
 
 import pygame as pg
+import time
 
 delta = {
     pg.K_UP : (0,-1),
@@ -13,7 +14,7 @@ delta = {
 
 
 
-def check_bound(obj_rct: pg.Rect , scr_rct: pg.Rect) -> tuple[bool,bool]:
+def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect ) -> tuple[bool,bool]:
     yoko, tate = True, True
     if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right: 
         yoko = False
@@ -29,6 +30,9 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img_damaged = pg.image.load("ex02/fig/8.png")
+    kk_img_right = pg.image.load("ex02/fig/3.png")
+    kk_img_left = pg.transform.flip(kk_img_right, True, False)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900,400
     bomb_img = pg.Surface((20, 20))
@@ -46,7 +50,7 @@ def main():
                 return 0
 
         tmr += 1
-
+        screen.blit(bg_img, [0, 0])
         key_lst = pg.key.get_pressed()
         for k, mv in delta.items():
             if key_lst[k]:
@@ -55,7 +59,23 @@ def main():
             for k, mv in delta.items():
                 if key_lst[k]:
                     kk_rct.move_ip(-mv[0],-mv[1])
-        screen.blit(bg_img, [0, 0])
+        # Set kk_image image based on direction
+        if vx > 0:
+            kk_img = kk_img_right
+        else:
+            kk_img = kk_img_left
+        # Increase size and speed of bomb over time
+        if tmr % 1000 == 0:
+            bb_rct.inflate_ip(5, 5)
+            vx, vy = vx * 1.2, vy * 1.2
+        
+        if kk_rct.colliderect(bb_rct):
+            kk_img = kk_img_damaged
+            screen.blit(kk_img, kk_rct)
+            pg.display.update()
+            time.sleep(3)
+            return
+
         screen.blit(kk_img, kk_rct) # 4 
         bb_rct.move_ip(vx,vy)
         yoko, tate =check_bound(screen.get_rect(),bb_rct)
@@ -64,8 +84,7 @@ def main():
         if not tate:
             vy*=-1
         screen.blit(bomb_img,bb_rct)
-        if kk_rct.colliderect(bb_rct):
-            return
+        
 
 
         pg.display.update()

38a2afff6b8c3c54e7f14a9948d19a07e12ac628
ahmed <c0b21010af@edu.teu.ac.jp>
2023-04-25 15:53:36 +0900

    pre-finish the 5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,16 @@ delta = {
 
 
 
+
+def check_bound(obj_rct: pg.Rect , scr_rct: pg.Rect) -> tuple[bool,bool]:
+    yoko, tate = True, True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right: 
+        yoko = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom: 
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -41,14 +51,22 @@ def main():
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
-
-
+        if check_bound(screen.get_rect(),kk_rct) != (True,True):
+            for k, mv in delta.items():
+                if key_lst[k]:
+                    kk_rct.move_ip(-mv[0],-mv[1])
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct) # 4 
         bb_rct.move_ip(vx,vy)
+        yoko, tate =check_bound(screen.get_rect(),bb_rct)
+        if not yoko: 
+            vx*= -1
+        if not tate:
+            vy*=-1
         screen.blit(bomb_img,bb_rct)
-        draw_sfc = pg.Surface((100,100))
-        pg.draw.circle(draw_sfc, (255,0,0), (50,50), 50)
+        if kk_rct.colliderect(bb_rct):
+            return
+
 
         pg.display.update()
         clock.tick(1000)

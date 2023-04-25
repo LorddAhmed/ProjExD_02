4f62e3c9a65ec6a31197bbfec81eca7b4da02989
ahmed <c0b21010af@edu.teu.ac.jp>
2023-04-25 14:32:35 +0900

    finish the 3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,13 +12,13 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     bomb_img = pg.Surface((20, 20))
     pg.draw.circle(bomb_img, (255, 0, 0), (10, 10), 10)
-    bomb_img.fill((255, 0, 0))
     bomb_img.set_colorkey((0, 0, 0))
     x,y = random.randint(0,1600), random.randint(0,900)
-    
-
-
+    vx,vy = +1,+1
+    bb_rct = bomb_img.get_rect()
+    bb_rct.center = x,y
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -27,7 +27,8 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bomb_img, [x, y])
+        bb_rct.move_ip(vx,vy)
+        screen.blit(bomb_img,bb_rct)
         draw_sfc = pg.Surface((100,100))
         pg.draw.circle(draw_sfc, (255,0,0), (50,50), 50)
 

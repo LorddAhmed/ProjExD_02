41662bf72d319af324b08262fca6c1a12f26f7a1
ahmed <c0b21010af@edu.teu.ac.jp>
2023-04-25 14:12:17 +0900

    finish the first

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -8,8 +8,11 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bomb_img = pg.Surface((20, 20))
+    bomb_img.fill((255, 0, 0))
+    bomb_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_img, (255, 0, 0), (10, 10), 10)
     tmr = 0
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -18,6 +21,9 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bomb_img, [600, 200])
+        draw_sfc = pg.Surface((100,100))
+        pg.draw.circle(draw_sfc, (255,0,0), (50,50), 50)
 
         pg.display.update()
         clock.tick(1000)
